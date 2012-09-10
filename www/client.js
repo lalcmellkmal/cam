d820aa1fa7fa8bebda8d43ae02575bc09ab95621
Lal'C Mellk Mal <lalc@doushio.com>
2012-09-10 03:40:42 -0700

    Fade in new black card

--- www/client.js
+++ www/client.js
@@ -178,7 +178,7 @@ var GameView = Backbone.View.extend({
 		var $black = this.$('.black:first');
 		if (black) {
 			var info = parseBlack(black);
-			$black.find('a').text(info.text);
+			$black.find('a').text(info.text).hide().fadeIn();
 			this.model.set({blackInfo: info});
 		}
 		else {
